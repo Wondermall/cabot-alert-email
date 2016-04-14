df1c842a34f582ba72a8fb012bb51eb449386820
romankor <kournjaev@gmail.com>
2016-04-14 16:25:32 -0700

    rewrite plugin to use sparkpost

--- cabot_alert_email/models.py
+++ cabot_alert_email/models.py
@@ -2,6 +2,7 @@ from os import environ as env
 from django.conf import settings
 from cabot.cabotapp.alert import AlertPlugin
 from sparkpost import SparkPost
+from django.template import Context, Template
 import logging
 
 email_template = """Service {{ service.name }} {{ scheme }}://{{ host }}{% url 'service' pk=service.id %} {% if service.overall_status != service.PASSING_STATUS %}alerting with status: {{ service.overall_status }}{% else %}is back to normal{% endif %}.
@@ -28,28 +29,21 @@ class EmailSPAlert(AlertPlugin):
         if service.overall_status != service.PASSING_STATUS:
             if service.overall_status == service.CRITICAL_STATUS:
                 emails += [u.email for u in users if u.email]
-            subject = '%s status for service: %s' % (
-                service.overall_status, service.name)
+            subject = '%s status for service: %s' % (service.overall_status, service.name)
         else:
             subject = 'Service back to normal: %s' % (service.name,)
-
+        c = Context({
+            'service': service,
+            'host': settings.WWW_HTTP_HOST,
+            'scheme': settings.WWW_SCHEME
+        })
         sp = SparkPost(env.get('SPARKPOST_API_KEY'))
 
         response = sp.transmissions.send(
             recipients=emails,
-            html=email_template,
-            text=email_template,
+            text=t.render(c),
             from_email='Cabot <%s>' % env.get('CABOT_FROM_EMAIL'),
             subject=subject,
-            metadata={
-                'key': 'value',
-                'arbitrary': 'values'
-            },
-            substitution_data={
-                'service': service,
-                'host': settings.WWW_HTTP_HOST,
-                'scheme': settings.WWW_SCHEME
-            },
             reply_to='no-reply@int.wmmails.com'
         )
         logging.info(response)
